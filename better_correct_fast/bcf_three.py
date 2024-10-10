290a528ac7bfcfb63098742b98e2fc558048b6a4
Boy d'Hont <boydhont@gmail.com>
2024-10-10 12:27:01 +0200

    Added default snapshot file retrieval

--- better_correct_fast/bcf_three.py
+++ better_correct_fast/bcf_three.py
@@ -5,13 +5,18 @@
 # https://github.com/IfcOpenShell/IfcOpenShell/blob/v0.8.0/src/bcf/bcf/v3/bcfxml.py
 
 # TODO does not work with snapshot yet. Snapshots are part of the viewport element with the name "snapshot.jpg". It will be included in the archive under the folder named after top_guid. It is supposed to be saved in the ViewPoint, under "Snapshot", then the text as "snapshot.jpg"
+# TODO delete this icon.png later on
 
 import bcf.v3.bcfxml as BCF # type: ignore
+
+from pathlib import Path
 
 def create(title="BetterCorrectFast: Check this out!", description="Automatically generated topic by BetterCorrectFast", project_name=""): # TODO come up with a better alias # TODO come up with better placeholder values
     
     bcf = BCF.BcfXml().create_new(project_name)
     topic = _add_topic(bcf, title, description)
+
+    default_snapshot_path = _get_default_snapshot_path() 
 
     return bcf
 
@@ -26,6 +31,15 @@ def _add_topic(bcf, title, description, author="BetterCorrectFast", topic_type="
     )
 
     return topic
+
+def _get_default_snapshot_path():
+    
+    current_file = Path(__file__).resolve()
+    snapshot_path = current_file.parent / 'assets' / 'icon.jpg'
+
+    print(f"The default snapshot file is being used {'and is located' if snapshot_path.exists() else ', but was not found'} at: {snapshot_path}") # User feedback
+    
+    return snapshot_path
 
 def save(bcf, filename):
     bcf.save(filename)
